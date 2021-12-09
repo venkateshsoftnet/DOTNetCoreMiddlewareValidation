40e471a6af4883c4cd80a718a0fae276a75bdba6
Venkatesh Thirupathi <venkateshjsoft@gmail.com>
2021-12-09 08:16:13 +0400

    Add project files.

---
+++ RoomService.WebAPI/PasswordCheckerMiddleware.cs
@@ -0,0 +1,32 @@
+﻿using System.Net;
+using System.Threading.Tasks;
+
+using Microsoft.AspNetCore.Http;
+
+namespace RoomService.WebAPI
+{
+    public class PasswordCheckerMiddleware
+    {
+        private readonly RequestDelegate _next;
+        public PasswordCheckerMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task InvokeAsync(HttpContext context)
+        {
+
+            var passwordKey = context.Request.Headers["passwordKey"];
+            if (string.IsNullOrWhiteSpace(passwordKey) ||
+                passwordKey != "passwordKey123456789")
+            {
+                context.Response.StatusCode = (int)HttpStatusCode.Forbidden;
+
+                return;
+            }
+
+            // Call the next delegate/middleware in the pipeline
+            await _next(context);
+        }
+    }
+}
